23031db1ee18cc098811b2872411e17fa4b719ad
belongtothenight <106849935+belongtothenight@users.noreply.github.com>
2024-03-16 14:06:07 +0800

    Auto generate setup.py as well

--- hw1_traffic_pcap_parser/cython_run.ps1
+++ hw1_traffic_pcap_parser/cython_run.ps1
@@ -1,4 +1,13 @@
+# Generate setup.py for cython
+"import distutils.core
+import Cython.Build
+
+distutils.core.setup(ext_modules = Cython.Build.cythonize(`"traffic_pcap_parser.pyx`"))" > setup.py
 C:\Python312\python.exe .\setup.py build_ext --inplace
+
+# Generate execute.py to execute the compiled code
 "import traffic_pcap_parser" > execute.py
 C:\Python312\python.exe .\execute.py
+
 rm execute.py
+rm setup.py
