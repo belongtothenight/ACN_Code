a9924bd5a16aeda197c5439696916facf80b33d0
belongtothenight <106849935+belongtothenight@users.noreply.github.com>
2024-04-27 21:25:39 +0800

    Populate echo message

--- hw4_libtrace_setup/remove.sh
+++ hw4_libtrace_setup/remove.sh
@@ -15,8 +15,9 @@ load_preset "./config.ini"
 # ====================================================================================
 # Backup files
 # ====================================================================================
+msg="backup"
 if [ $task_uthash == 1 ] || [ $task_libwandder == 1 ] || [ $task_wandio == 1 ] || [ $task_libtrace == 1 ]; then
-    echo_notice "$this_script" "base" "Backing up files"
+    echo_notice "$this_script" "$msg" "Backing up files"
     sudo cp -rf "${system_include_dir}/" "${program_install_dir}/"
     sudo mv "${program_install_dir}/include/" "${program_install_dir}/backup_include/"
     sudo cp -rf "${system_lib_dir}/" "${program_install_dir}/"
@@ -26,8 +27,9 @@ fi
 # ====================================================================================
 # Remove uthash
 # ====================================================================================
+msg="remove uthash"
 if [ $task_uthash == 1 ]; then
-    echo_notice "$this_script" "base" "Removing uthash"
+    echo_notice "$this_script" "$msg" "Removing uthash"
     sudo rm -rf "${program_install_dir}/${uthash_name}"
     sudo rm -f ${system_include_dir}/ut*.h
 fi
@@ -35,8 +37,9 @@ fi
 # ====================================================================================
 # Remove libwandder
 # ====================================================================================
+msg="remove libwandder"
 if [ $task_libwandder == 1 ]; then
-    echo_notice "$this_script" "base" "Removing libwandder"
+    echo_notice "$this_script" "$msg" "Removing libwandder"
     sudo rm -f ${system_include_dir}/libwandder*.h
     sudo rm -f ${system_lib_dir}/libwandder*
     sudo rm -f "${program_install_dir}/${libwandder_file}"
@@ -47,8 +50,9 @@ fi
 # ====================================================================================
 # Remove wandio
 # ====================================================================================
+msg="remove wandio"
 if [ $task_wandio == 1 ]; then
-    echo_notice "$this_script" "base" "Removing wandio"
+    echo_notice "$this_script" "$msg" "Removing wandio"
     sudo rm -f ${system_include_dir}/wandio*.h
     sudo rm -f ${system_lib_dir}/libwandio*
     sudo rm -f "${program_install_dir}/${wandio_file}"
@@ -59,8 +63,9 @@ fi
 # ====================================================================================
 # Remove libtrace
 # ====================================================================================
+msg="remove libtrace"
 if [ $task_libtrace == 1 ]; then
-    echo_notice "$this_script" "base" "Removing libtrace"
+    echo_notice "$this_script" "$msg" "Removing libtrace"
     sudo rm -f ${system_include_dir}/libtrace*.h
     sudo rm -f ${system_include_dir}/libpacketdump*.h
     sudo rm -rf "${system_include_dir}/${libtrace_name}"
@@ -74,10 +79,12 @@ fi
 # ====================================================================================
 # Remove libtrace tutorial repo
 # ====================================================================================
+msg="remove libtrace tutorial"
 if [ $task_libtrace_tutorial == 1 ]; then
-    echo_notice "$this_script" "base" "Removing libtrace tutorial repo"
+    echo_notice "$this_script" "$msg" "Removing libtrace tutorial repo"
     sudo rm -rf "${program_install_dir}/libtrace_tutorial"
 fi
 
 # End of file
-echo_notice "$this_script" "base" "Cleanup complete"
+msg="cleanup"
+echo_notice "$this_script" "$msg" "Cleanup complete"
--- hw4_libtrace_setup/setup.sh
+++ hw4_libtrace_setup/setup.sh
@@ -25,87 +25,89 @@ load_preset "./config.ini"
 # ====================================================================================
 # Download source code
 # ====================================================================================
+msg="download"
 if [ $task_uthash == 1 ]; then
-    echo_notice "$this_script" "base" "Cloning uthash repo"
+    echo_notice "$this_script" "$msg" "Cloning uthash repo"
     if [ $script_stat == "dev" ]; then
-        err_conti_exec "sudo git clone ${uthash_repo_url} ${program_install_dir}/${uthash_name}" "${this_script}" "base"
+        err_conti_exec "sudo git clone ${uthash_repo_url} ${program_install_dir}/${uthash_name}" "${this_script}" "$msg"
     elif [ $script_stat == "prod" ]; then
-        err_retry_exec "sudo git clone ${uthash_repo_url} ${program_install_dir}/${uthash_name}" 1 5 "${this_script}" "base" 1
+        err_retry_exec "sudo git clone ${uthash_repo_url} ${program_install_dir}/${uthash_name}" 1 5 "${this_script}" "$msg" 1
     fi
 fi
 
 if [ $task_libwandder == 1 ]; then
-    echo_notice "$this_script" "base" "Downloading libwandder release"
+    echo_notice "$this_script" "$msg" "Downloading libwandder release"
     if [ $script_stat == "dev" ]; then
-        err_conti_exec "sudo wget $wget_flags --output-document ${program_install_dir}/${libwandder_file} ${libwandder_release_url}" "${this_script}" "base"
+        err_conti_exec "sudo wget $wget_flags --output-document ${program_install_dir}/${libwandder_file} ${libwandder_release_url}" "${this_script}" "$msg"
     elif [ $script_stat == "prod" ]; then
-        err_retry_exec "sudo wget $wget_flags --output-document ${program_install_dir}/${libwandder_file} ${libwandder_release_url}" 1 5 "${this_script}" "base" 1
+        err_retry_exec "sudo wget $wget_flags --output-document ${program_install_dir}/${libwandder_file} ${libwandder_release_url}" 1 5 "${this_script}" "$msg" 1
     fi
 fi
 
 if [ $task_wandio == 1 ]; then
-    echo_notice "$this_script" "base" "Downloading wandio release"
+    echo_notice "$this_script" "$msg" "Downloading wandio release"
     if [ $script_stat == "dev" ]; then
-        err_conti_exec "sudo wget $wget_flags --output-document ${program_install_dir}/${wandio_file} ${wandio_release_url}" "${this_script}" "base"
+        err_conti_exec "sudo wget $wget_flags --output-document ${program_install_dir}/${wandio_file} ${wandio_release_url}" "${this_script}" "$msg"
     elif [ $script_stat == "prod" ]; then
-        err_retry_exec "sudo wget $wget_flags --output-document ${program_install_dir}/${wandio_file} ${wandio_release_url}" 1 5 "${this_script}" "base" 1
+        err_retry_exec "sudo wget $wget_flags --output-document ${program_install_dir}/${wandio_file} ${wandio_release_url}" 1 5 "${this_script}" "$msg" 1
     fi
 fi
 
 if [ $task_libtrace == 1 ]; then
-    echo_notice "$this_script" "base" "Cloning libtrace repo"
+    echo_notice "$this_script" "$msg" "Cloning libtrace repo"
     if [ $script_stat == "dev" ]; then
-        err_conti_exec "sudo wget $wget_flags --output-document ${program_install_dir}/${libtrace_file} ${libtrace_release_url}" "${this_script}" "base"
+        err_conti_exec "sudo wget $wget_flags --output-document ${program_install_dir}/${libtrace_file} ${libtrace_release_url}" "${this_script}" "$msg"
     elif [ $script_stat == "prod" ]; then
-        err_retry_exec "sudo wget $wget_flags --output-document ${program_install_dir}/${libtrace_file} ${libtrace_release_url}" 1 5 "${this_script}" "base" 1
+        err_retry_exec "sudo wget $wget_flags --output-document ${program_install_dir}/${libtrace_file} ${libtrace_release_url}" 1 5 "${this_script}" "$msg" 1
     fi
 fi
 
 if [ $task_libtrace_tutorial == 1 ]; then
-    echo_notice "$this_script" "base" "Cloning libtrace tutorial repo"
+    echo_notice "$this_script" "$msg" "Cloning libtrace tutorial repo"
     if [ $script_stat == "dev" ]; then
-        err_conti_exec "sudo git clone ${libtrace_turorial_repo_url} ${program_install_dir}/libtrace_tutorial" "${this_script}" "base"
+        err_conti_exec "sudo git clone ${libtrace_turorial_repo_url} ${program_install_dir}/libtrace_tutorial" "${this_script}" "$msg"
     elif [ $script_stat == "prod" ]; then
-        err_retry_exec "sudo git clone ${libtrace_turorial_repo_url} ${program_install_dir}/libtrace_tutorial" 1 5 "${this_script}" "base" 1
+        err_retry_exec "sudo git clone ${libtrace_turorial_repo_url} ${program_install_dir}/libtrace_tutorial" 1 5 "${this_script}" "$msg" 1
     fi
 fi
 
 # ====================================================================================
 # Install dependencies with dpkg
 # ====================================================================================
+msg="install dependencies"
 if [ $task_wandio == 1 ]; then
-    echo_notice "$this_script" "base" "Installing wandio dependencies"
+    echo_notice "$this_script" "$msg" "Installing wandio dependencies"
     if [ $script_stat == "dev" ]; then
         :
     elif [ $script_stat == "prod" ]; then
-        err_retry_exec "aptins automake"                1 5 "${this_script}" "base" 1 # >= 1.9
-        err_retry_exec "aptins libpthread-stubs0-dev"   1 5 "${this_script}" "base" 1 # (optional)
-        err_retry_exec "aptins zlib1g-dev"              1 5 "${this_script}" "base" 1 # (optional)
-        err_retry_exec "aptins libbz2-dev"              1 5 "${this_script}" "base" 1 # (optional)
-        err_retry_exec "aptins liblzma-dev"             1 5 "${this_script}" "base" 1 # (optional)
-        err_retry_exec "aptins liblzo2-dev"             1 5 "${this_script}" "base" 1 # (optional)
-        err_retry_exec "aptins liblz4-dev"              1 5 "${this_script}" "base" 1 # (optional)
-        err_retry_exec "aptins libzstd-dev"             1 5 "${this_script}" "base" 1 # (optional)
+        err_retry_exec "aptins automake"                1 5 "${this_script}" "$msg" 1 # >= 1.9
+        err_retry_exec "aptins libpthread-stubs0-dev"   1 5 "${this_script}" "$msg" 1 # (optional)
+        err_retry_exec "aptins zlib1g-dev"              1 5 "${this_script}" "$msg" 1 # (optional)
+        err_retry_exec "aptins libbz2-dev"              1 5 "${this_script}" "$msg" 1 # (optional)
+        err_retry_exec "aptins liblzma-dev"             1 5 "${this_script}" "$msg" 1 # (optional)
+        err_retry_exec "aptins liblzo2-dev"             1 5 "${this_script}" "$msg" 1 # (optional)
+        err_retry_exec "aptins liblz4-dev"              1 5 "${this_script}" "$msg" 1 # (optional)
+        err_retry_exec "aptins libzstd-dev"             1 5 "${this_script}" "$msg" 1 # (optional)
     fi
 fi
 
 if [ $task_libtrace == 1 ]; then
-    echo_notice "$this_script" "base" "Installing libtrace dependencies"
+    echo_notice "$this_script" "$msg" "Installing libtrace dependencies"
     if [ $script_stat == "dev" ]; then
         :
     elif [ $script_stat == "prod" ]; then
-        err_retry_exec "aptins automake"                1 5 "${this_script}" "base" 1 # >= 1.9, done
-        err_retry_exec "aptins libpcap-dev"             1 5 "${this_script}" "base" 1 # >= 0.8
-        err_retry_exec "aptins flex"                    1 5 "${this_script}" "base" 1
-        err_retry_exec "aptins bison"                   1 5 "${this_script}" "base" 1
-        err_retry_exec "aptins pkg-config"              1 5 "${this_script}" "base" 1
-        #err_retry_exec "aptins libwandio-dev"           1 5 "${this_script}" "base" 1 # build from source
-        err_retry_exec "aptins libyaml-dev"             1 5 "${this_script}" "base" 1 # (optional)
-        err_retry_exec "aptins libssl-dev"              1 5 "${this_script}" "base" 1 # (optional) for libcrypto
-        err_retry_exec "aptins libncurses5-dev"         1 5 "${this_script}" "base" 1 # (optional) for libncurses
-        err_retry_exec "aptins libncursesw5-dev"        1 5 "${this_script}" "base" 1 # (optional) for libncurses
+        err_retry_exec "aptins automake"                1 5 "${this_script}" "$msg" 1 # >= 1.9, done
+        err_retry_exec "aptins libpcap-dev"             1 5 "${this_script}" "$msg" 1 # >= 0.8
+        err_retry_exec "aptins flex"                    1 5 "${this_script}" "$msg" 1
+        err_retry_exec "aptins bison"                   1 5 "${this_script}" "$msg" 1
+        err_retry_exec "aptins pkg-config"              1 5 "${this_script}" "$msg" 1
+        #err_retry_exec "aptins libwandio-dev"           1 5 "${this_script}" "$msg" 1 # build from source
+        err_retry_exec "aptins libyaml-dev"             1 5 "${this_script}" "$msg" 1 # (optional)
+        err_retry_exec "aptins libssl-dev"              1 5 "${this_script}" "$msg" 1 # (optional) for libcrypto
+        err_retry_exec "aptins libncurses5-dev"         1 5 "${this_script}" "$msg" 1 # (optional) for libncurses
+        err_retry_exec "aptins libncursesw5-dev"        1 5 "${this_script}" "$msg" 1 # (optional) for libncurses
     else
-        echo_error "$this_script" "base" "Unknown script_stat"
+        echo_error "$this_script" "$msg" "Unknown script_stat"
         exit 1
     fi
 fi
@@ -113,8 +115,9 @@ fi
 # ====================================================================================
 # Source code preparation
 # ====================================================================================
+msg="source code preparation"
 if [ $task_libwandder == 1 ] || [ $task_wandio == 1 ] || [ $task_libtrace == 1 ]; then
-    echo_notice "$this_script" "base" "Extracting .tar.gz files"
+    echo_notice "$this_script" "$msg" "Extracting .tar.gz files"
     if [ $task_libwandder == 1 ]; then
         sudo tar $tar_flags "${program_install_dir}/${libwandder_file}" -C "${program_install_dir}"
     fi
@@ -124,34 +127,34 @@ if [ $task_libwandder == 1 ] || [ $task_wandio == 1 ] || [ $task_libtrace == 1 ]
     if [ $task_libtrace == 1 ]; then
         sudo tar $tar_flags "${program_install_dir}/${libtrace_file}" -C "${program_install_dir}"
     fi
-    echo_notice "$this_script" "base" "Creating symbolic links"
+    echo_notice "$this_script" "$msg" "Creating symbolic links"
     if [ $task_libwandder == 1 ]; then
         if [ $script_stat == "dev" ]; then
-            err_conti_exec "sudo ln -s ${program_install_dir}/${libwandder_ln} ${program_install_dir}/${libwandder_name}" "${this_script}" "base"
+            err_conti_exec "sudo ln -s ${program_install_dir}/${libwandder_ln} ${program_install_dir}/${libwandder_name}" "${this_script}" "$msg"
         elif [ $script_stat == "prod" ]; then
             sudo ln -s "${program_install_dir}/${libwandder_ln}" "${program_install_dir}/${libwandder_name}"
         else
-            echo_error "$this_script" "base" "Unknown script_stat"
+            echo_error "$this_script" "$msg" "Unknown script_stat"
             exit 1
         fi
     fi
     if [ $task_wandio == 1 ]; then
         if [ $script_stat == "dev" ]; then
-            err_conti_exec "sudo ln -s ${program_install_dir}/${wandio_ln} ${program_install_dir}/${wandio_name}" "${this_script}" "base"
+            err_conti_exec "sudo ln -s ${program_install_dir}/${wandio_ln} ${program_install_dir}/${wandio_name}" "${this_script}" "$msg"
         elif [ $script_stat == "prod" ]; then
             sudo ln -s "${program_install_dir}/${wandio_ln}" "${program_install_dir}/${wandio_name}"
         else
-            echo_error "$this_script" "base" "Unknown script_stat"
+            echo_error "$this_script" "$msg" "Unknown script_stat"
             exit 1
         fi
     fi
     if [ $task_libtrace == 1 ]; then
         if [ $script_stat == "dev" ]; then
-            err_conti_exec "sudo ln -s ${program_install_dir}/${libtrace_ln} ${program_install_dir}/${libtrace_name}" "${this_script}" "base"
+            err_conti_exec "sudo ln -s ${program_install_dir}/${libtrace_ln} ${program_install_dir}/${libtrace_name}" "${this_script}" "$msg"
         elif [ $script_stat == "prod" ]; then
             sudo ln -s "${program_install_dir}/${libtrace_ln}" "${program_install_dir}/${libtrace_name}"
         else
-            echo_error "$this_script" "base" "Unknown script_stat"
+            echo_error "$this_script" "$msg" "Unknown script_stat"
             exit 1
         fi
     fi
@@ -160,13 +163,14 @@ fi
 # ====================================================================================
 # Building & Installing
 # ====================================================================================
+msg="building & installing"
 if [ $task_uthash == 1 ]; then
-    echo_notice "$this_script" "base" "Copying uthash headers to system include path"
+    echo_notice "$this_script" "$msg" "Copying uthash headers to system include path"
     sudo cp ${program_install_dir}/${uthash_name}/src/* "${system_include_dir}"
 fi
 
 if [ $task_libwandder == 1 ]; then
-    echo_notice "$this_script" "base" "Building libwandder"
+    echo_notice "$this_script" "$msg" "Building libwandder"
     cd "${program_install_dir}/${libwandder_name}"
     sudo ./bootstrap.sh
     sudo ./configure
@@ -175,7 +179,7 @@ if [ $task_libwandder == 1 ]; then
 fi
 
 if [ $task_wandio == 1 ]; then
-    echo_notice "$this_script" "base" "Building wandio"
+    echo_notice "$this_script" "$msg" "Building wandio"
     cd "${program_install_dir}/${wandio_name}"
     sudo ./bootstrap.sh
     sudo ./configure
@@ -184,7 +188,7 @@ if [ $task_wandio == 1 ]; then
 fi
 
 if [ $task_libtrace == 1 ]; then
-    echo_notice "$this_script" "base" "Building libtrace"
+    echo_notice "$this_script" "$msg" "Building libtrace"
     cd "${program_install_dir}/${libtrace_name}"
     sudo ./bootstrap.sh
     sudo ./configure LDFLAGS=-L/usr/local/lib CPPFLAGS=-I/usr/local/include
@@ -198,16 +202,17 @@ if [ $task_libtrace_tutorial == 1 ]; then
     libtrace_tutorial_codedemo="${program_install_dir}/${libtrace_tutorial_name}/codedemo"
     #LD_LIBRARY_PATH="${program_install_dir}/${libwandder_name}/src:${program_install_dir}/${wandio_name}/src"
     LD_LIBRARY_PATH="${system_lib_dir}:${system_include_dir}:${libtrace_tutorial_lib_massdal}:$LD_LIBRARY_PATH"
-    echo_notice "$this_script" "base" "Building libtrace tutorial -> lib/massdal"
+    echo_notice "$this_script" "$msg" "Building libtrace tutorial -> lib/massdal"
     cd $libtrace_tutorial_lib_massdal
     sudo make $make_flags
-    echo_notice "$this_script" "base" "Building libtrace tutorial -> hw0"
+    echo_notice "$this_script" "$msg" "Building libtrace tutorial -> hw0"
     cd $libtrace_tutorial_hw0
     sudo make $make_flags
-    echo_notice "$this_script" "base" "Building libtrace tutorial -> codedemo"
+    echo_notice "$this_script" "$msg" "Building libtrace tutorial -> codedemo"
     cd $libtrace_tutorial_codedemo
     sudo make $make_flags
 fi
 
 # End of file
-echo_notice "$this_script" "base" "Setup complete"
+msg="cleanup"
+echo_notice "$this_script" "$msg" "Setup complete"
